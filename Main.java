9cb30742ab224c60458834394fdfbca3fa30e669
kpanik51 <150745697+kpanik51@users.noreply.github.com>
2024-03-10 15:37:00 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // Launch the JavaFX application
+        javafx.application.Application.launch(GroceryListApp.class, args);
+    }
+}
